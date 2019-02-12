ea772ace5fe33d771e2e1e5a9301cc20cb48431e
momoNorthy <northy360@126.com>
2019-02-12 14:01:03 +0800

    x

---
+++ momoTest2/main.cpp
@@ -0,0 +1,9 @@
+#include"momoLib/Lib.h"
+int main()
+{
+    gift cout<<gift endl;//#define gift std::
+    MOMO _ran_ a(gift time(nullptr));
+    MOMO _rti_ u(19,21);
+    gift cout<<u(a) <<gift endl;
+    return 0;
+}
